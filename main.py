528444c6ee8045279664329e74664a7cd2cdc560
James Winston <kenjameswinston@gmail.com>
2022-04-25 09:34:37 -0400

    Simplify extraction logic for readability

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from colorgram import colorgram
+from turtle import Turtle, Screen
 
 
-# return list of tuple objects extracted from an image
+# returns a list of tuple objects extracted from an image
 def extract_color(file, num_colors):
     colors = colorgram.extract('fruit_salad.png', num_colors)
     color_list = []
     for color in colors:
-        rgb = color.rgb
-        color_list.append(tuple(rgb))
-
+        r = color.rgb.r
+        g = color.rgb.g
+        b = color.rgb.b
+        new_color = (r, g, b)
+        color_list.append(new_color)
     return color_list
 
+# create a list of 30 rgb color tuples
+extracted_colors = extract_color('fruit_salad.png', 30)
 
-extracted_colors = extract_color('fruit_salad.png', 8)
-
-for rgb in extracted_colors:
-    print(rgb)
+print(extracted_colors)
 
 
